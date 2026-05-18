b221a167eb031edab60a7cd880e631e1239bfde4
Camilla Marina <camilla.marina@aluno.ifsp.edu.br>
2026-05-18 08:38:32 -0300

    projeto finalizado

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { prisma } from '../libs/prisma'
-import { createUser, createUsers, getAllUsers } from '../services/user';
+import { createUser, createUsers, getAllUsers, getUserByEmail } from '../services/user';
 
 
 export const mainRouter = Router();
@@ -51,6 +51,15 @@ mainRouter.get('/users', async(req, res) => {
         res.json({ users })
     } else {
         res.status(500).json({ error: 'Error fetching users'})
+    }
+})
+
+mainRouter.get('/user', async (req, res) => {
+    const user = await getUserByEmail('charlie.brown@exemple.com')
+    if (user) {
+        res.json({ user })
+    } else {
+        res.status(404).json({ error: 'User not found'})
     }
 })
 
